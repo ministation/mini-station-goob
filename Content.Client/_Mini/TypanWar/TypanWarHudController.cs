0418f48982dd78b594f8e43c69130b65e60f5112
Egorick1845 <proegorweb@gmail.com>
2026-07-11 19:57:06 +0300

    лицензия

--- Content.Client/_Mini/TypanWar/TypanWarHudController.cs
+++ Content.Client/_Mini/TypanWar/TypanWarHudController.cs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2026 Egorik1
+// Мини-станция, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/ministation/mini-station-goob/master/LICENSE.TXT
+
 using Content.Client.Gameplay;
 using Content.Client.UserInterface.Systems.Gameplay;
 using Content.Shared._Mini.TypanWar;
--- Content.Server/_Mini/TypanWar/TypanWarArmorySystem.cs
+++ Content.Server/_Mini/TypanWar/TypanWarArmorySystem.cs
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2026 Egorik1
+// Мини-станция, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/ministation/mini-station-goob/master/LICENSE.TXT
+
 using Content.Server.Station.Systems;
 using Content.Shared._Mini.TypanWar;
 using Content.Shared.Lock;
