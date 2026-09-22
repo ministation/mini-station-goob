a64223f16a43867298d57028901048a60cbdb3b4
ИИ-сисадмин <sysadmin@ministation.ru>
2026-09-22 23:49:32 +0300

    Починены два файла после разбора конфликтов: возвращены потерянные переводы строк перед закрывающими скобками, в LatheSystem.Goob.cs сохранены оба метода

--- Content.Server/Lathe/LatheSystem.Goob.cs
+++ Content.Server/Lathe/LatheSystem.Goob.cs
@@ -1,4 +1,12 @@
+using System.Linq;
+using Content.Goobstation.Common.NTR.Scan;
+using Content.Goobstation.Shared.Lathe;
+using Content.Server.Chat.Systems;
+using Content.Server.Lathe.Components;
+using Content.Shared.Chemistry.Components;
+using Content.Shared.Chemistry.Reagent;
 using Content.Shared.Lathe;
+using Content.Shared.Materials;
 
 namespace Content.Server.Lathe;
 
@@ -29,6 +37,56 @@ public sealed partial class LatheSystem
         batch.ItemsPrinted++;
         if (batch.ItemsPrinted >= batch.ItemsRequested || batch.ItemsPrinted < 0)
             comp.Queue.RemoveFirst();
+
+        comp.CurrentRecipe = recipe;
+        return true;
+    }
+
+
+    /// <summary>
+    /// Produces 0-time items that output into the storage automatically.
+    /// Used in order to prevent stack overflows (of the server) caused by printing a lot of materials at once.
+    /// TODO It's still not great and in the future should be replaced with something even more optimized.
+    /// </summary>
+    private void FinishProducingManyStorage(Entity<LatheComponent, LatheProducingComponent> ent)
+    {
+        var (uid, comp, prodComp) = ent;
+
+        if (comp.CurrentRecipe != null)
+        {
+            var count = comp.Queue.Count;
+            while (comp.CurrentRecipe != null)
+            {
+                var batch = comp.Queue.First();
+                batch.ItemsPrinted++;
+                if (batch.ItemsPrinted >= batch.ItemsRequested || batch.ItemsPrinted < 0)
+                    comp.Queue.RemoveFirst();
+
+                // Modified FinishProducing method
+                var currentRecipe = _proto.Index(comp.CurrentRecipe.Value);
+                if (currentRecipe.Result is { } resultProto)
+                {
+                    var prototype = _proto.Index(resultProto);
+                    // Storage output is already true if we are in this method
+                    if (prototype.TryGetComponent<PhysicalCompositionComponent>(out var composition, _factory))
+                    {
+                        _materialStorage.TryChangeMaterialAmount(uid, composition.MaterialComposition);
+                    }
+                    else
+                    {
+                        // This case should ideally never happen? But whatever
+                        var result = Spawn(resultProto, Transform(uid).Coordinates);
+                        _stack.TryMergeToContacts(result);
+                        if (TryComp<ScannableForPointsComponent>(result, out var scannable)) // Goobstation
+                            scannable.Points = 0; // Goobstation, this thing is to prevent ntr duping points via an emagged lathe
+                    }
+                }
+
+                if (currentRecipe.ResultReagents is { } resultReagents &&
+                    comp.ReagentOutputSlotId is { } slotId)
+                {
+                    var toAdd = new Solution(
+                        resultReagents.Select(p => new ReagentQuantity(p.Key.Id, p.Value)));
 
                     // dispense it in the container if we have it and dump it if we don't
                     if (_container.TryGetContainer(uid, slotId, out var container) &&
@@ -66,7 +124,5 @@ public sealed partial class LatheSystem
             UpdateUserInterfaceState(uid, comp);
             UpdateRunningAppearance(uid, false);
         }
-
-        comp.CurrentRecipe = recipe;
-        return true;    }
+    }
 }
